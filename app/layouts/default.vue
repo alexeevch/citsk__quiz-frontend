117f4a2c044cbcd35bedcaf19a7afa9220aaf7a1
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-17 21:59:30 +0300

    feat: Добавлен базовый layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="app-layout">
+    <header>header</header>
+    <main class="main">
+      <NuxtPage />
+    </main>
+    <footer>footer</footer>
+  </div>
+</template>
