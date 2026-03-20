965eea8d4da19be97060be35f4220cd30227dee1
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-20 09:44:57 +0300

    feat: Заглушка страницы аутентификации

---
+++ app/layouts/auth.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { useLayout } from "~/composables/useLayout";
+
+const layout = useLayout();
+</script>
+
+<template>
+  <div class="auth">
+    <header class="auth__header">
+      <Button text rounded severity="secondary" @click="layout.toggleDarkMode">
+        <i :class="layout.settings.value.isDarkMode ? 'pi pi-sun' : 'pi pi-moon'" />
+      </Button>
+    </header>
+    <main class="auth__main">
+      <NuxtPage />
+    </main>
+    <footer class="auth__footer">footer</footer>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "~/assets/styles/common/mixins" as mixins;
+
+.auth {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100dvh;
+  padding: 0 var(--padding-card-md);
+
+  @include mixins.screensFrom("sm") {
+    padding: 0 var(--padding-card-sm);
+  }
+
+  &__header,
+  &__footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 1rem 0;
+  }
+
+  &__header {
+    color: var(--color-text-gray);
+  }
+
+  &__main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+  }
+
+  &__footer {
+    color: var(--color-text-gray);
+  }
+}
+</style>
---
+++ app/pages/auth/sign-in.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+const appName = useRuntimeConfig().public.appName;
+definePageMeta({
+  layout: "auth"
+});
+</script>
+
+<template>
+  <div class="sign-in">
+    <div class="sign-in__branding">
+      <div class="sign-in__logo">
+        <NuxtImg width="80" src="/images/logo-primary.png" />
+      </div>
+      <div class="sign-in__title">
+        <h1 class="sign-in__label">Аутентификация</h1>
+        <p class="sign-in__app-name">{{ appName }}</p>
+      </div>
+    </div>
+    <Form v-slot="$form" class="sign-in__form" :initialValues :resolver @submit="onFormSubmit">
+      <div class="sign-in__form-field">
+        <InputText name="login" type="text" placeholder="Email" fluid />
+      </div>
+      <div class="sign-in__form-field">
+        <Password name="password" placeholder="Password" :feedback="false" fluid toggle-mask />
+      </div>
+      <Button icon="pi pi-sign-in" label="Войти" />
+    </Form>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.test {
+  color: var(--color-text-gray);
+}
+.sign-in {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: var(--padding-card-md);
+
+  &__branding {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: var(--padding-card-sm);
+    text-align: center;
+  }
+
+  &__form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: var(--padding-card-sm);
+    padding: var(--padding-card-sm);
+    border-radius: var(--border-radius-primary);
+    background-color: var(--color-bg-card);
+  }
+}
+</style>
