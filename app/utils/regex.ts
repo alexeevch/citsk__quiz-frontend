fba05b1b097e5371bb4fa263f24811aef537def4
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-23 15:15:59 +0300

    feat: Добавлены утилиты регулярных выражений

---
+++ app/utils/regex.ts
@@ -0,0 +1,7 @@
+export const PHONE_REGEX = /^(\+7|8)?[\s-]?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2}$/;
+
+export const DATE_REGEX = /^(\d{2})\.(\d{2})\.(\d{4})$/;
+
+export const SYMBOLS_REGEX = /[^\w\s]/g;
+
+export const UPPERCASE_REGEX = /[A-Z]/;
