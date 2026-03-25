2e45af573c95617aa1a1089a361e73d224d677da
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-25 13:23:34 +0300

    feat: Обновлены типы организации

--- app/types/api/Organization.ts
+++ app/types/api/Organization.ts
@@ -1,8 +1,10 @@
-export type Organization = {
-  id: string;
+type BaseOrganization = {
   name: string;
   phone: string;
   email: string;
   site_url?: string;
   address?: string;
 };
+export type OrganizationData = BaseOrganization & { id: number };
+export type OrganizationCreateDTO = BaseOrganization;
+export type OrganizationUpdateDTO = Partial<BaseOrganization>;
