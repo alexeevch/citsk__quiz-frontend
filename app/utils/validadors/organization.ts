8338a56a209561b769e0766ff2ee8a939e6c0a20
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-26 20:24:59 +0300

    feat: Валидация полей формы организаций

---
+++ app/utils/validadors/organization.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import { emailSchema } from "~/utils/validadors/shared";
+
+export const organizationSchema = z.object({
+  name: z.string().min(1, "Введите название"),
+  phone: z
+    .string()
+    .trim()
+    .min(1, { message: "Введите номер телефона" })
+    .refine((val) => PHONE_REGEX.test(val), {
+      message: "Введите корректный номер телефона"
+    }),
+  email: emailSchema,
+  site_url: z.url("Введите корректный URL").optional().or(z.literal("")),
+  address: z.string().optional()
+});
