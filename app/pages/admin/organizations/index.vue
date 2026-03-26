945aa8ff3c90e229d3b96b65e919baff8fd08691
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-26 20:26:29 +0300

    feat: Административная страница организаций

---
+++ app/pages/admin/organizations/index.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const appName = useRuntimeConfig().public.appName;
+const pageName = "Организации";
+
+useSeoMeta({
+  title: `${pageName} - ${appName}`
+});
+
+definePageMeta({
+  permissions: [
+    "api.view_organization",
+    "api.add_organization",
+    "api.change_organization",
+    "api.delete_organization"
+  ],
+  permissionMode: "all",
+  breadcrumb: pageName
+});
+</script>
+
+<template>
+  <div class="organization-page">
+    <AdminOrganizationSection>
+      <template #title>
+        <h1 class="organization-page__title">{{ pageName }}</h1>
+      </template>
+    </AdminOrganizationSection>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.organization-page {
+  &__title {
+    margin: 0;
+    padding: 0;
+  }
+}
+</style>
