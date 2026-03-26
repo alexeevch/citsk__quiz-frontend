8be08af2a9c5a19556e270a90f15c8d02e057bb9
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-26 20:27:20 +0300

    feat: Обновлены элементы меню приложения

--- app/app.config.ts
+++ app/app.config.ts
@@ -13,138 +13,48 @@ export default defineAppConfig({
         ]
       },
       {
+        label: "Администрирование",
+        items: [
+          {
+            key: "admin-users",
+            icon: "pi pi-users",
+            label: "Пользователи",
+            to: "/admin/users"
+          },
+          {
+            key: "admin-organizations",
+            icon: "pi pi-building",
+            label: "Организации",
+            to: "/admin/organizations"
+          }
+        ]
+      },
+      {
         label: "Викторины",
         items: [
           {
             key: "quiz",
             label: "Викторины",
             icon: "pi pi-list-check",
-            children: [
-              {
-                key: "quiz-list",
-                label: "Список викторин",
-                icon: "pi pi-list",
-                to: "/quiz"
-              },
-              {
-                key: "quiz-create",
-                label: "Создать викторину",
-                icon: "pi pi-plus",
-                to: "/quiz/create"
-              }
-            ]
+            to: "/quiz"
           },
           {
-            key: "question",
-            label: "Вопросы",
-            icon: "pi pi-question-circle",
-            children: [
-              {
-                key: "question-list",
-                label: "Список вопросов",
-                icon: "pi pi-list",
-                to: "/questions"
-              },
-              {
-                key: "question-create",
-                label: "Создать вопрос",
-                icon: "pi pi-plus",
-                to: "/questions/create"
-              }
-            ]
-          },
-          {
-            key: "answer",
-            label: "Ответы",
-            icon: "pi pi-check-circle",
-            children: [
-              {
-                key: "answer-list",
-                label: "Список ответов",
-                icon: "pi pi-list",
-                to: "/answers"
-              },
-              {
-                key: "answer-create",
-                label: "Создать ответ",
-                icon: "pi pi-plus",
-                to: "/answers/create"
-              }
-            ]
-          }
-        ]
-      },
-      {
-        label: "Результаты",
-        items: [
-          {
-            key: "userresult",
-            label: "Результаты викторин",
+            key: "quiz-stat",
+            label: "Статистика",
             icon: "pi pi-chart-bar",
-            children: [
-              {
-                key: "userresult-list",
-                label: "Все результаты",
-                icon: "pi pi-list",
-                to: "/results"
-              }
-            ]
+            to: "/quiz/stat"
           },
           {
-            key: "useranswer",
+            key: "quiz-answers",
             label: "Ответы пользователей",
             icon: "pi pi-comments",
-            children: [
-              {
-                key: "useranswer-list",
-                label: "Все ответы",
-                icon: "pi pi-list",
-                to: "/user-answers"
-              }
-            ]
-          }
-        ]
-      },
-      {
-        label: "Администрирование",
-        items: [
-          {
-            key: "customuser",
-            label: "Пользователи",
-            icon: "pi pi-users",
-            children: [
-              {
-                key: "customuser-list",
-                label: "Список пользователей",
-                icon: "pi pi-list",
-                to: "/users"
-              },
-              {
-                key: "customuser-create",
-                label: "Создать пользователя",
-                icon: "pi pi-user-plus",
-                to: "/users/create"
-              }
-            ]
+            to: "/quiz/user-answers"
           },
           {
-            key: "organization",
-            label: "Организации",
-            icon: "pi pi-building",
-            children: [
-              {
-                key: "organization-list",
-                label: "Список организаций",
-                icon: "pi pi-list",
-                to: "/organizations"
-              },
-              {
-                key: "organization-create",
-                label: "Создать организацию",
-                icon: "pi pi-plus",
-                to: "/organizations/create"
-              }
-            ]
+            key: "quiz-winners",
+            label: "Победители",
+            icon: "pi pi-sparkles",
+            to: "/quiz/winners"
           }
         ]
       }
