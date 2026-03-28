ee87a1a3329b6c9c16f0765bc64a07e93d158409
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-28 16:25:45 +0300

    feat: Обновлены типы викторин

--- app/types/api/Quiz.ts
+++ app/types/api/Quiz.ts
@@ -2,7 +2,7 @@ import type { OrganizationData } from "~/types/api/Organization";
 
 type BaseQuiz = {
   name: string;
-  description: string;
+  description?: string;
   total_questions: number;
   is_active: boolean;
 };
@@ -10,3 +10,8 @@ type BaseQuiz = {
 export type QuizData = BaseQuiz & { id: number; organization: OrganizationData };
 export type QuizCreateDTO = BaseQuiz & { organization_id: number };
 export type QuizUpdateDTO = Partial<QuizCreateDTO>;
+
+export type QuizQueryParams = {
+  is_active?: boolean;
+  organization?: number;
+};
