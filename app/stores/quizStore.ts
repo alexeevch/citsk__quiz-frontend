df3006fc707563a7b1a185f70b76c18c4929686d
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-30 10:41:22 +0300

    feat: Обработка прав в сторе квиза

--- app/stores/quizStore.ts
+++ app/stores/quizStore.ts
@@ -4,40 +4,56 @@ import type { QuizCreateDTO, QuizData, QuizQueryParams, QuizUpdateDTO } from "~/
 
 export const useQuizStore = defineStore("quiz", () => {
   const { $repositories } = useNuxtApp();
+  const authStore = useAuthStore();
   const { error, isLoading, load } = useStoreRequest();
 
   const quizList = ref<QuizData[]>([]);
 
   const fetchQuizzes = async (query?: QuizQueryParams) => {
-    try {
-      const response = await load<QuizData[]>(() => $repositories.quiz.getAll(query));
-      quizList.value = response;
-
-      return response;
-    } catch (e) {
-      error.value = "Не удалось загрузить список викторин";
-      throw e;
+    if (!authStore.can("api.view_quiz")) {
+      const message = "Недостаточно прав на просмотр викторин";
+      error.value = message;
+      throw new Error(message);
     }
+
+    const response = await load<QuizData[]>(() => $repositories.quiz.getAll(query));
+    quizList.value = response;
+    return response;
   };
 
   const createQuiz = async (dto: QuizCreateDTO) => {
+    if (!authStore.can("api.add_quiz")) {
+      const message = "Недостаточно прав на создание викторины";
+      error.value = message;
+      throw new Error(message);
+    }
     return await load<QuizData>(() => $repositories.quiz.create(dto));
   };
 
   const updateQuiz = async (id: number, dto: QuizUpdateDTO) => {
+    if (!authStore.can("api.change_quiz")) {
+      const message = "Недостаточно прав на редактирование викторины";
+      error.value = message;
+      throw new Error(message);
+    }
     return await load<QuizData>(() => $repositories.quiz.update(id, dto));
   };
 
   const deleteQuiz = async (id: number) => {
+    if (!authStore.can("api.delete_quiz")) {
+      const message = "Недостаточно прав на удаление викторины";
+      error.value = message;
+      throw new Error(message);
+    }
     return await load(() => $repositories.quiz.delete(id));
   };
 
   const showQuiz = async (id: number) => {
-    return await $repositories.quiz.update(id, { is_active: true });
+    return await updateQuiz(id, { is_active: true });
   };
 
   const hideQuiz = async (id: number) => {
-    return await $repositories.quiz.update(id, { is_active: false });
+    return await updateQuiz(id, { is_active: false });
   };
 
   return {
