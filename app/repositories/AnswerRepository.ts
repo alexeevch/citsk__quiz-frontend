edb3ce4c50ca1e7c2e734620d5ef50eea47b8f01
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-03-31 15:37:23 +0300

    feat: Добавлен репозиторий ответов

---
+++ app/repositories/AnswerRepository.ts
@@ -0,0 +1,36 @@
+import { BaseRepository } from "./BaseRepository";
+import type { AnswerCreateDTO, AnswerData, AnswerUpdateDTO } from "~/types/api/Answer";
+
+class AnswerRepository extends BaseRepository {
+  private readonly resource = "/v1/admin/questions/";
+
+  getAll(): Promise<AnswerData[]> {
+    return this.call<AnswerData[]>(this.resource);
+  }
+
+  getById(id: number): Promise<AnswerData> {
+    return this.call<AnswerData>(this.resource + id + "/");
+  }
+
+  create(dto: AnswerCreateDTO): Promise<AnswerData> {
+    return this.call<AnswerData>(this.resource, {
+      method: "POST",
+      body: dto
+    });
+  }
+
+  update(id: number, dto: AnswerUpdateDTO): Promise<AnswerData> {
+    return this.call<AnswerData>(`${this.resource}${id}/`, {
+      method: "PATCH",
+      body: dto
+    });
+  }
+
+  delete(id: number): Promise<undefined> {
+    return this.call(this.resource + id + "/", {
+      method: "DELETE"
+    });
+  }
+}
+
+export default AnswerRepository;
