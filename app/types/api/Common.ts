f15f05bdba74cfd256d38b970bc90280f39fc419
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-04-03 15:25:19 +0300

    feat: Вспомогательные типы для Api

---
+++ app/types/api/Common.ts
@@ -0,0 +1,18 @@
+export interface ApiResponseCollection<T> {
+  items: T[];
+  meta: ApiPaginator;
+}
+
+export type ApiPaginator = {
+  total: number;
+  per_page: number;
+  current_page: number;
+  last_page: number;
+  from: number;
+  to: number;
+  path: string;
+  next_page_url: string | null;
+  prev_page_url: string | null;
+};
+
+export type ApiQueryOrder<T extends string> = T | `-${T}`;
