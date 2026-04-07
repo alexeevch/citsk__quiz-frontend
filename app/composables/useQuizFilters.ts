b572be4e77f78b4df16783553efc1f639e0be4d7
Mikhail Kucherenko <2miklmiklmikl2@gmail.com>
2026-04-07 13:23:44 +0300

    feat: Доработан useQuizFilters.ts

--- app/composables/useQuizFilters.ts
+++ app/composables/useQuizFilters.ts
@@ -1,11 +1,17 @@
-import type { QuizQueryParams } from "~/types/api/Quiz";
+import type { QuizQueryFilter } from "~/types/api/Quiz";
 
 type activityFilter = {
   label: string;
   value?: boolean;
 };
 
-export function useQuizFilters() {
+const DEFAULT_FILTERS: QuizQueryFilter = {
+  order: "-id",
+  is_active: undefined,
+  organization: undefined
+};
+
+export function useQuizFilters(onFiltersChange?: () => void) {
   const route = useRoute();
   const router = useRouter();
 
@@ -22,30 +28,44 @@ export function useQuizFilters() {
     }
   ]);
 
-  const filters = computed<QuizQueryParams>(() => {
+  const filters = computed<QuizQueryFilter>(() => {
     return {
       organization: isAdmin.value
         ? deserializeParam(route.query.organization, "number")
         : user.value?.organization.id,
-      is_active: deserializeParam(route.query.is_active, "boolean"),
-      order: deserializeParam(route.query.order, "string") ?? "-id"
+      is_active: deserializeParam(route.query.is_active, "boolean") ?? DEFAULT_FILTERS.is_active,
+      order: deserializeParam(route.query.order, "string") ?? DEFAULT_FILTERS.order
     };
   });
 
-  function setFilters(newFilters: Partial<QuizQueryParams>) {
+  async function setFilters(newFilters: Partial<QuizQueryFilter>) {
     const serializedQuery = serializeQuery(newFilters);
 
     if (!isAdmin.value) {
       serializedQuery.organization = user.value?.organization.id;
     }
 
-    router.replace({
+    await router.replace({
       query: {
         ...route.query,
         ...serializedQuery
       }
     });
+
+    onFiltersChange?.();
   }
 
-  return { activityFilters, filters, setFilters };
+  async function resetFilters() {
+    await router.replace({
+      query: serializeQuery({
+        ...route.query,
+        ...DEFAULT_FILTERS,
+        organization: isAdmin.value ? DEFAULT_FILTERS.organization : user.value?.organization.id
+      })
+    });
+
+    onFiltersChange?.();
+  }
+
+  return { activityFilters, filters, setFilters, resetFilters };
 }
